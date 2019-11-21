81a1757e4ff7d5908a37df937d521ba0d6e162dc
liudan6 <liudan6@staff.sina.com.cn>
2019-11-21 18:46:33 +0800

    新增两个方法输出

--- test/demo.js
+++ test/demo.js
@@ -4,11 +4,17 @@ const mainAction = require('../lib/mainAction');
 const params = require('./params');
 const testContent = require('./testContent');
 
+// 获取当前环境变量
+console.log('当前环境变量:' + mainAction.getCurrentEnv(params));
+
+// 获取当前环境params
+console.log('当前环境params：');
+console.log(mainAction.getCurrentEnvParams(params));
+
 console.log('params-replace执行前：');
 console.log(testContent);
-console.log('\n');
 
-const newCont = mainAction(JSON.stringify(testContent), params);
+const newCont = mainAction.action(JSON.stringify(testContent), params);
 
 console.log('params-replace执行后：');
 console.log(newCont);
